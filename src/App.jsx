baf85b6abbcfed167b2ed4eb37028965de64cef7
Giventus <weishen735@gmail.com>
2024-11-20 12:09:17 +0700

    next

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import PostsList from "./components/PostsList";
+import MainHeader from "./components/MainHeader";
+import { useState } from "react";
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
   return (
     <>
-      <PostsList />
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostsList
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
     </>
   );
 }
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,28 +2,36 @@ import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 import { useState } from "react";
+import Modal from "./Modal";
 
-export default function PostsList() {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-  function changeBodyHandler(event) {
-    setEnteredBody(event.target.value);
-    console.log(event.target.value);
-  }
-  function changeAuthorHandler(event) {
-    setEnteredAuthor(event.target.value);
-    console.log(event.target.value);
+export default function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
   return (
     <>
-      <NewPost
-        bodyChange={changeBodyHandler}
-        authorChange={changeAuthorHandler}
-      />
-      <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
-        <Post author="Given lagi" body="keren juga kamu bang" />
-      </ul>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {/* <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Given lagi" body="keren juga kamu bang" /> */}
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
